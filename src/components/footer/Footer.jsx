b0e06967c40c9082ab006656f9bacfd2abc8c2d9
AVenturi98 <lssndrventuri@gmail.com>
2024-11-29 23:15:54 +0100

    ADD: footer style module

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,11 @@
+import style from './Footer.module.css'
+
 function Footer() {
 
     return (
         <>
         <footer>
-            <div className="main__footer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum fuga iure, architecto velit debitis laudantium eius inventore nemo blanditiis optio, voluptas quos, perferendis ea dolore. Aperiam, rerum cumque? Veniam, soluta.</div>
+            <div className={style.mainFooter}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum fuga iure, architecto velit debitis laudantium eius inventore nemo blanditiis optio, voluptas quos, perferendis ea dolore. Aperiam, rerum cumque? Veniam, soluta.</div>
         </footer>
         </>
     )
