da159dea71fb17197d97986f2540cad017f2781b
AVenturi98 <lssndrventuri@gmail.com>
2024-11-29 23:13:23 +0100

    ADD: card style module

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,20 +1,20 @@
 import image from '../../assets/img.jpg'
+import style from './Card.module.css'
+
 
 function Card() {
     
     return (
         <>
         <main className="container">
-            <div className="row">
-                <div className="card">
-                    <div className="card_img">
-                        <img className="image" src={image} alt="" />
-                    </div>
-                    <div className="card-body">
-                        <div className="title_card"> Spider-Man 3</div>
-                        <div className="description_card">Spider-Man 3 è un film del 2007 diretto da Sam Raimi. La trama del film è incentrata sulla vita di Spiderman, che sembra aver subito una svolta positiva sotto tutti i punti di vista.</div>
-                        <button className='btn'>Leggi di più</button>
-                    </div>
+            <div className={style.card}>
+                <div className={style.cardImg}>
+                    <img className={style.image}src={image} alt="" />
+                </div>
+                <div className={style.cardBody}>
+                    <div className={style.titleCard}> Spider-Man 3</div>
+                    <div className={style.descriptionCard}>Spider-Man 3 è un film del 2007 diretto da Sam Raimi. La trama del film è incentrata sulla vita di Spiderman, che sembra aver subito una svolta positiva sotto tutti i punti di vista.</div>
+                    <button className={style.btn}>Leggi di più</button>
                 </div>
             </div>
         </main>
